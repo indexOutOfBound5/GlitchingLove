1a31187e70b4a9f975564b1713d038d8a3a5dbf0
Meghan Halton <meghalton@gmail.com>
2014-05-10 15:39:03 +0100

    Update to add main character movement

--- main.lua
+++ main.lua
@@ -1,15 +1,53 @@
 STRICT = true
 DEBUG = true
-
 require 'zoetrope'
-
+jumpValue = -100
+jumpHeight = 30
+stillJumping = 0
 the.app = App:new
 {
-	onNew = function (self)
-		self:add(Text:new
-		{
-			y = 300, width = the.app.width, align = 'center',
-			text = 'Welcome to Zoetrope! Edit main.lua to get started.'
-		})
-	end
+    onRun = function (self)
+        self.player = Fill:new
+        {
+            x = 0, y = 0,
+            width = 32, height = 48,
+            fill = {0, 0, 255},
+            acceleration = { y = 600 },
+            onUpdate = function (self, elapsed)
+                if the.keys:pressed('left') then
+                    self.velocity.x = -100
+                elseif the.keys:pressed('right') then
+                    self.velocity.x = 100
+                elseif the.keys:pressed('up') then
+                    onJump(self)
+                else
+                    self.velocity.x = 0
+                end
+            end
+        }
+        self:add(self.player)
+
+        self.platform = Fill:new
+        {
+            x = 0, y = 400,
+            width = 128, height = 32,
+            fill = {255, 255, 255},
+            onCollide = function (self, other, xOverlap, yOverlap)
+                stillJumping = 0;
+                self:displace(other)
+                other.velocity.y = 0
+            end
+        }
+        self:add(self.platform)
+    end,
+ 
+    onUpdate = function (self, elapsed)
+        self.platform:collide(self.player)
+    end
 }
+function onJump(self)
+	if stillJumping <= jumpHeight then
+		self.velocity.y = jumpValue
+		stillJumping = stillJumping + 1
+	end
+end
