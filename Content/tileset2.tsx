dcc2762c4b4e8e1f7d2e7df48da0ca9b7c755c54
Elazar Trosman <elazar55@hotmail.com>
2014-05-10 17:46:20 +0100

    Level1

---
+++ Content/tileset2.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset2" tilewidth="60" tileheight="60">
+ <image source="tiles.png" trans="ff00ff" width="360" height="180"/>
+ <tile id="0">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="solid" value="true"/>
+  </properties>
+ </tile>
+</tileset>
