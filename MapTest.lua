3ceedefb9f6b0bed5a8b604f45b50af6a0146750
Meghan Halton <meghalton@gmail.com>
2014-05-10 17:27:44 +0100

    Add code to interperet map tiles

--- MapTest.lua
+++ MapTest.lua
@@ -3,6 +3,11 @@ MapTest = View:extend
 	onNew = function (self)
 		self:loadLayers('Content/maptest.lua')
 		self.focus = the.player
---		self:clampTo(self.map)
+		self:clampTo(self.map)
+	end,
+
+	onUpdate = function (self)
+		self.map:subdisplace(the.player)
+--		self.objects:collide(self.objects)
 	end
 }
